1f051e08162a3d21b3c38aafa7ad49f8c43dcb2d
hemantsumant2166-creator <hemantsumant2166@gmail.com>
2025-09-18 21:46:45 -0700

    Add files via upload

---
+++ lll.py
@@ -0,0 +1,24 @@
+'''a=6
+for i in range(1,11):
+    print(a,"*",i,"=",a*i)'''
+
+'''i=int(input("enter the no.:-"))
+while i<=99:
+    print(i)
+    i+=10'''
+
+'''a=int(input("enter the marks:-"))
+if 90>a>80:
+     print("grade=A+")
+elif 80>a>70:
+     print("grade=A")
+elif 70>a>60:
+     print("grade=B")
+elif 33<a:
+     print("fail")'''
+
+def hemant():
+    print("saud")
+    print("hamid")
+hemant()
+    
